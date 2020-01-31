875bc83e4b787cdcb4c3876fb3287235cec30218
Thyago Mendes <thymendes@gmail.com>
2020-01-31 17:53:20 -0300

    limpeza

--- src/services/facebook.js
+++
@@ -1,9 +0,0 @@
-require('./server')
-require =('dotenv/config')
-
-FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      var accessToken = response.authResponse.accessToken;
-    } 
-    console.log(getLoginStatus)
-  } );
