d6933068a359c2ecc8138aa737c5e64367f69628
Thyago Mendes <thymendes@gmail.com>
2020-01-31 18:50:42 -0300

    rota fix

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 const ResumeController = require ('./controllers/ResumeController')
 //const FacebookController = require ('./controllers/FacebookController')
 
-routes.get('/resume', ResumeController.consulta)
+routes.get('/api/curriculo', ResumeController.consulta)
 //routes.get ('/facebookapi', FacebookController.faceapi)
 
 module.exports = routes 
